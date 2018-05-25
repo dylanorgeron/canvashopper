8fb976355df31dcef6ae356d80f85648a5d31052
dylan orgeron <dylan.orgeron@gmail.com>
2018-05-25 15:51:35 -0500

    offsets

--- scripts/tile.ts
+++ scripts/tile.ts
@@ -1,4 +1,4 @@
-import {emitter, canvas} from './index' 
+import {emitter, canvas, level} from './index' 
 
 class Tile {
 	public readonly w = 50
@@ -7,7 +7,11 @@ class Tile {
 	public x = this.col * this.w
 	public y = this.row * this.h
   
-	constructor(public col: number, public row: number, public isSolid: boolean) {  
+	constructor(
+		public col: number,
+		public row: number,
+		public isSolid: boolean
+	) {  
 		emitter.on('update', this.update.bind(this))
 	}
 
@@ -16,11 +20,21 @@ class Tile {
 	}
 
 	draw(){
-		var thickness = 1
-		canvas.canvasCTX.fillStyle = '#DDD'
-	  	canvas.canvasCTX.fillRect(this.x - (thickness), this.y - (thickness), this.w + (thickness * 2), this.h + (thickness * 2))
-	  	canvas.canvasCTX.fillStyle = this.isSolid ? '#aaa' : '#FFF'
-		canvas.canvasCTX.fillRect(this.x, this.y, this.w, this.h)
+		//determine where to draw the tile
+		const fallsShort = ((this.col + 1) * this.w) - level.offsetX < 0
+		if(!fallsShort){
+			var thickness = 1
+			canvas.canvasCTX.fillStyle = '#DDD'
+			canvas.canvasCTX.fillRect(this.x - (thickness), this.y - (thickness), this.w + (thickness * 2), this.h + (thickness * 2))
+			canvas.canvasCTX.fillStyle = this.isSolid ? '#aaa' : '#FFF'
+			canvas.canvasCTX.fillRect(this.x, this.y, this.w, this.h)
+		}else{
+			var thickness = 1
+			canvas.canvasCTX.fillStyle = '#DDD'
+			canvas.canvasCTX.fillRect(this.x - (thickness), this.y - (thickness), this.w + (thickness * 2), this.h + (thickness * 2))
+			canvas.canvasCTX.fillStyle = '#ff0000'
+			canvas.canvasCTX.fillRect(this.x, this.y, this.w, this.h)
+		}
 	}
 }
 
